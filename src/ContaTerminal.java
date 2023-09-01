d8db310b0c532c2e82e6f97d2a05df68e10a6921
domflp <109469999+domflp@users.noreply.github.com>
2023-09-01 20:32:49 -0300

    Salvando novamente

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args)  {
 
-        int numero   
+        int numero;   
         String agencia;
         String nome;
         double saldo;
